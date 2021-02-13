1b56f541c2bde2020b31bdaa5861e63f9535cd3c
Emmanuel´s Lap <segcro7@gmail.com>
2021-02-13 09:35:09 -0600

    Initial Commit

---
+++ DelaApp/DelaApp/Models/Invoice.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DelaApp.Models
+{
+    public class Invoice
+    {
+        public int InvoiceID { get; set; }
+        public string Folio { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Date { get; set; }
+        public int Percentage { get; set; }
+    }
+}
